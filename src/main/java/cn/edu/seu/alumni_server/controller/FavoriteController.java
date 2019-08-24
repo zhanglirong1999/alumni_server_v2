95107e9f27667a010eb6b46ed473ccb676c4f214
wangqing <2623898554@qq.com>
2019-08-24 15:00:29 +0800

    A 收藏

---
+++ src/main/java/cn/edu/seu/alumni_server/controller/FavoriteController.java
@@ -0,0 +1,55 @@
+package cn.edu.seu.alumni_server.controller;
+
+import cn.edu.seu.alumni_server.controller.dto.common.WebResponse;
+import cn.edu.seu.alumni_server.dao.entity.Favorite;
+import cn.edu.seu.alumni_server.dao.mapper.FavoriteMapper;
+import com.github.pagehelper.PageHelper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/v2")
+@SuppressWarnings("ALL")
+public class FavoriteController {
+
+    @Autowired
+    FavoriteMapper favoriteMapper;
+
+    @GetMapping("/favorite")
+    WebResponse getFavorite(@RequestParam Long accountId,
+                            @RequestParam int pageIndex,
+                            @RequestParam int pageSize) {
+        Favorite favorite = new Favorite();
+        favorite.setAccountId(accountId);
+        PageHelper.startPage(pageIndex, pageSize);
+        List<Favorite> res = favoriteMapper.select(favorite);
+        return new WebResponse().success(res);
+    }
+
+    @PostMapping("/favorite")
+    WebResponse changeFavoriteStatus(@RequestBody Map req) {
+        Long accountId = (Long) req.get("accountId");
+        Long favoriteAccountId = (Long) req.get("favoriteAccountId");
+        Integer status = (Integer) req.get("status");
+
+        Favorite favorite = new Favorite();
+        favorite.setAccountId(accountId);
+        favorite.setFavoriteAccountId(favoriteAccountId);
+        favorite.setStatus(status);
+        if (favoriteMapper.select(favorite).size() == 0) {
+            favoriteMapper.insertSelective(favorite);
+        } else {
+            Example example = new Example(Favorite.class);
+            Example.Criteria criteria = example.createCriteria();
+            criteria.andEqualTo("accountId", accountId);
+            criteria.andEqualTo("favoriteAccountId", favoriteAccountId);
+
+            favoriteMapper.updateByExampleSelective(favorite, example);
+        }
+        return new WebResponse();
+    }
+}
---
+++ src/main/java/cn/edu/seu/alumni_server/dao/mapper/FavoriteMapper.java
@@ -0,0 +1,7 @@
+package cn.edu.seu.alumni_server.dao.mapper;
+
+import cn.edu.seu.alumni_server.dao.entity.Favorite;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface FavoriteMapper extends Mapper<Favorite> {
+}
