e534205ae62cc5de8f23dd8275da5a43f5f662d9
lei_jiang <lei_jiang@baofu.com>
2019-09-03 19:25:15 +0800

    喜欢关系实体类、dao

---
+++ src/main/java/cn/edu/seu/alumni_server/dao/mapper/LikeMapper.java
@@ -0,0 +1,9 @@
+package cn.edu.seu.alumni_server.dao.mapper;
+
+import cn.edu.seu.alumni_server.dao.entity.Like;
+import org.springframework.stereotype.Repository;
+import tk.mybatis.mapper.common.Mapper;
+
+@Repository
+public interface LikeMapper extends Mapper<Like> {
+}
