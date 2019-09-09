c325a1a1452197a47a948a5ca92b06aca01b1ad0
wangqing <2623898554@qq.com>
2019-09-09 23:12:51 +0800

    F 之前db 和dto数据格式不一致，故有个转换， 现在同意了，这些代码就多余了

--- src/main/java/cn/edu/seu/alumni_server/controller/dto/JobDTO.java
+++ src/main/java/cn/edu/seu/alumni_server/controller/dto/JobDTO.java
@@ -18,15 +18,11 @@ public class JobDTO {
 
     public JobDTO(Job job) {
         BeanUtils.copyProperties(job, this);
-        this.setStartTime(job.getStartTime());
-        this.setEndTime(job.getEndTime());
     }
 
     public Job toJob() {
         Job job = new Job();
         BeanUtils.copyProperties(this, job);
-        job.setStartTime(this.getStartTime());
-        job.setEndTime(this.getEndTime());
         return job;
     }
 }
