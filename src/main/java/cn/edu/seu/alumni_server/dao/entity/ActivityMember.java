b5c22ce78d28904501e0c38109138c570bc41bf1
ParanoidRoot <578791680@qq.com>
2019-10-25 00:03:02 +0800

    :bug: Fix bug.

--- src/main/java/cn/edu/seu/alumni_server/dao/entity/ActivityMember.java
+++ src/main/java/cn/edu/seu/alumni_server/dao/entity/ActivityMember.java
@@ -2,15 +2,16 @@ package cn.edu.seu.alumni_server.dao.entity;
 
 import lombok.Data;
 
+import javax.persistence.Id;
 import java.io.Serializable;
 
 @Data
 public class ActivityMember implements Serializable {
+    @Id
     private Long activityId;
 
+    @Id
     private Long accountId;
-
-    private Boolean validStatus;
 
     private Boolean readStatus;
 
