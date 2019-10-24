79c25fab0161542b5b49abe3b6d8f49f0d7cf6cb
ParanoidRoot <578791680@qq.com>
2019-10-25 00:02:29 +0800

    :white_check_mark: Finish activities' construction.

--- src/main/java/cn/edu/seu/alumni_server/dao/mapper/ActivityMapper.java
+++ src/main/java/cn/edu/seu/alumni_server/dao/mapper/ActivityMapper.java
@@ -5,6 +5,15 @@ import cn.edu.seu.alumni_server.dao.entity.Activity;
 import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
+import java.util.HashMap;
+
 @Repository
 public interface ActivityMapper extends Mapper<Activity> {
+
+    /**
+     * 根据输入的活动 id 输出基本信息, 注意要输出基本
+     * @param activityId 活动 id.
+     * @return 查询的结果 json 对象.
+     */
+    HashMap<String, Object> getBasicInfos(Long activityId);
 }
