a314e6c5e4349cf4067d24f23a86f0e7e7189156
ParanoidRoot <578791680@qq.com>
2019-10-24 09:20:18 +0800

    :bug: Fix bug.

--- src/main/java/cn/edu/seu/alumni_server/dao/mapper/AlumniCircleMapper.java
+++ src/main/java/cn/edu/seu/alumni_server/dao/mapper/AlumniCircleMapper.java
@@ -1,6 +1,7 @@
 package cn.edu.seu.alumni_server.dao.mapper;
 
+import cn.edu.seu.alumni_server.dao.entity.AlumniCircle;
 import tk.mybatis.mapper.common.Mapper;
 
-public interface AlumniCircleMapper extends Mapper<AlumniCircleMapper> {
+public interface AlumniCircleMapper extends Mapper<AlumniCircle> {
 }
