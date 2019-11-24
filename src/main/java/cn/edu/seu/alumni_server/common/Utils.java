ffd48994561126ea5e4d32e2be2155700a1c5133
ParanoidRoot <578791680@qq.com>
2019-11-24 20:26:54 +0800

    :white_check_mark: Pass white checks.

--- src/main/java/cn/edu/seu/alumni_server/common/Utils.java
+++ src/main/java/cn/edu/seu/alumni_server/common/Utils.java
@@ -1,5 +1,13 @@
 package cn.edu.seu.alumni_server.common;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.apache.http.entity.ContentType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
 public class Utils {
 
     static IdGenerator idGenerator;
@@ -12,4 +20,11 @@ public class Utils {
     public static long generateId() {
         return idGenerator.nextId();
     }
+
+    public static MultipartFile fileToMultipartFile(File file) throws IOException {
+        FileInputStream inputStream = new FileInputStream(file);
+        return new MockMultipartFile(file.getName(), file.getName(),
+            ContentType.APPLICATION_OCTET_STREAM.toString(), inputStream);
+    }
+
 }
--- src/test/java/cn/edu/seu/alumni_server/AlumniServerApplicationTests.java
+++ src/test/java/cn/edu/seu/alumni_server/AlumniServerApplicationTests.java
@@ -1,16 +1,50 @@
 package cn.edu.seu.alumni_server;
 
+import cn.edu.seu.alumni_server.common.Utils;
+import cn.edu.seu.alumni_server.common.exceptions.ActivityServiceException;
+import cn.edu.seu.alumni_server.controller.dto.ActivityWithMultipartFileDTO;
+import cn.edu.seu.alumni_server.dao.entity.Activity;
+import cn.edu.seu.alumni_server.service.ActivityService;
+import cn.edu.seu.alumni_server.service.QCloudFileManager;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.multipart.MultipartFile;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = {AlumniServerApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AlumniServerApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Autowired
+	QCloudFileManager fileManager;
+	@Autowired
+	ActivityService activityService;
+
+	@Test
+	public void testPostImgs() {
+		// 获取图片
+		File file = new File(
+			"F:\\71118123\\照片\\ParanoidRoot.jpg"
+		);
+		try {
+			MultipartFile multipartFile = Utils.fileToMultipartFile(file);
+			ActivityWithMultipartFileDTO x = new ActivityWithMultipartFileDTO(
+				7647332456448L, null, null, null, null, null, null,
+				multipartFile, null, null, null, null, null, null, null
+			);
+			// 首先判断是否有效, 并且发送不为 null 的图片.
+			Activity ans = this.activityService.updateActivityDAO(x);
+			// 然后选择性更新.
+			this.activityService.updateActivity(ans);
+		} catch (IOException | IllegalAccessException | ActivityServiceException | InvocationTargetException e) {
+			e.printStackTrace();
+		}
+//		System.out.println("1123");
+	}
 
 }
