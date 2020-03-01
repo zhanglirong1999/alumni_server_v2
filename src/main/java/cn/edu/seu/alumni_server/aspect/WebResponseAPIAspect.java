91b6ca1c49290cce972622906d9eeb5f8932f955
ParanoidRoot <578791680@qq.com>
2020-03-01 19:38:06 +0800

    :art: Refactor response method.

---
+++ src/main/java/cn/edu/seu/alumni_server/aspect/WebResponseAPIAspect.java
@@ -0,0 +1,53 @@
+package cn.edu.seu.alumni_server.aspect;
+
+import cn.edu.seu.alumni_server.common.dto.WebResponseByObject;
+import cn.edu.seu.alumni_server.common.dto.WebServiceExceptionMessage;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class WebResponseAPIAspect {
+
+	@Pointcut("@annotation(cn.edu.seu.alumni_server.annotation.WebResponseAPIMethod)")
+	public void methodAPI() {
+	}
+
+	@Pointcut("@within(cn.edu.seu.alumni_server.annotation.WebResponseAPIController)")
+	public void typeAPI() {
+	}
+
+	@Around("methodAPI()")
+	public Object wrapResultForMethod(ProceedingJoinPoint proceedingJoinPoint) {
+		Object ans;
+		try {
+			ans = proceedingJoinPoint.proceed();
+		} catch (Throwable throwable) {
+			ans = new WebServiceExceptionMessage(throwable.getMessage());
+		}
+		return this.wrapWebResultToWebResponseByObject(ans);
+	}
+
+	@Around("typeAPI()")
+	public Object wrapResultForType(ProceedingJoinPoint proceedingJoinPoint) {
+		Object ans;
+		try {
+			ans = proceedingJoinPoint.proceed();
+		} catch (Throwable throwable) {
+			ans = new WebServiceExceptionMessage(throwable.getMessage());
+		}
+		return this.wrapWebResultToWebResponseByObject(ans);
+	}
+
+	private WebResponseByObject wrapWebResultToWebResponseByObject(Object ans) {
+		if (ans instanceof WebServiceExceptionMessage) {
+			return new WebResponseByObject().fail((WebServiceExceptionMessage) ans);
+		} else {
+			return new WebResponseByObject().success(ans);
+		}
+	}
+
+}
---
+++ src/main/java/cn/edu/seu/alumni_server/common/dto/WebResponseByObject.java
@@ -0,0 +1,39 @@
+package cn.edu.seu.alumni_server.common.dto;
+
+import cn.edu.seu.alumni_server.common.CONST;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class WebResponseByObject {
+
+	Integer status;
+	String message;
+	Object data;
+
+	public WebResponseByObject() {
+		this.status = CONST.SUCCESS_CODE;
+		this.message = CONST.SUCCESS_MESSAGE_DEFAULT;
+		this.data = null;
+	}
+
+	public WebResponseByObject success(Object _data) {
+		this.status = CONST.SUCCESS_CODE;
+		this.message = CONST.SUCCESS_MESSAGE_DEFAULT;
+		this.data = _data;
+		return this;
+	}
+
+	public WebResponseByObject fail(WebServiceExceptionMessage _message) {
+		return this.fail(_message.getMessage(), null);
+	}
+
+	public WebResponseByObject fail(String _message, Object _data) {
+		this.status = CONST.FAIL_CODE;
+		this.message = _message;
+		this.data = _data;
+		return this;
+	}
+
+}
