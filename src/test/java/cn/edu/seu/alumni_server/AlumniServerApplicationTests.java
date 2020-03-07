62bce52e3e27e51ec1c2d258fe8facc995588430
ParanoidRoot <578791680@qq.com>
2020-03-07 18:27:05 +0800

    :white_check_mark: Pass tests.

--- src/test/java/cn/edu/seu/alumni_server/AlumniServerApplicationTests.java
+++ src/test/java/cn/edu/seu/alumni_server/AlumniServerApplicationTests.java
@@ -2,17 +2,12 @@ package cn.edu.seu.alumni_server;
 
 import cn.edu.seu.alumni_server.common.Utils;
 import cn.edu.seu.alumni_server.common.dto.WebResponse;
-import cn.edu.seu.alumni_server.common.exceptions.ActivityServiceException;
 import cn.edu.seu.alumni_server.controller.CommonController;
-import cn.edu.seu.alumni_server.controller.dto.ActivityWithMultipartFileDTO;
-import cn.edu.seu.alumni_server.dao.entity.Activity;
-import cn.edu.seu.alumni_server.dao.entity.AlumniCircle;
 import cn.edu.seu.alumni_server.service.ActivityService;
 import cn.edu.seu.alumni_server.service.AlumniCircleService;
 import cn.edu.seu.alumni_server.service.QCloudFileManager;
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,28 +28,6 @@ public class AlumniServerApplicationTests {
 	CommonController commonController;
 	@Autowired
 	AlumniCircleService alumniCircleService;
-
-	@Test
-	public void testPostImgs() {
-		// 获取图片
-		File file = new File(
-			"F:\\JavaProjects\\AlumniServer\\doc\\demo.png"
-		);
-		try {
-			MultipartFile multipartFile = Utils.fileToMultipartFile(file);
-			ActivityWithMultipartFileDTO x = new ActivityWithMultipartFileDTO(
-				7647332456448L, null, null, null, null, null, null,
-				null, multipartFile, null, null, null, null, null, null
-			);
-			// 首先判断是否有效, 并且发送不为 null 的图片.
-			Activity ans = this.activityService.updateActivityDAO(x);
-			// 然后选择性更新.
-			this.activityService.updateActivity(ans);
-		} catch (IOException | IllegalAccessException | ActivityServiceException | InvocationTargetException e) {
-			e.printStackTrace();
-		}
-//		System.out.println("1123");
-	}
 
 	@Test
 	public void testUploadOneFile() {
@@ -93,7 +66,8 @@ public class AlumniServerApplicationTests {
 
 	@Test
 	public void testDeleteFile() {
-		System.out.println(this.commonController.deleteFile("https://root-test-bucket-1258701411.cos.ap-shanghai.myqcloud.com/11813611470848.jpg"));
+		System.out.println(this.commonController.deleteFile(
+			"https://root-test-bucket-1258701411.cos.ap-shanghai.myqcloud.com/11813611470848.jpg"));
 	}
 
 	@Test
