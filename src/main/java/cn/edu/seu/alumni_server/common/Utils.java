f22196cb67276da4cd1e47ce4534eaf9734c1500
ParanoidRoot <578791680@qq.com>
2020-03-23 00:52:28 +0800

    :arrow_down: Down to jdk 1.8 for compare two img.

--- src/main/java/cn/edu/seu/alumni_server/common/Utils.java
+++ src/main/java/cn/edu/seu/alumni_server/common/Utils.java
@@ -5,9 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Arrays;
 import java.util.Date;
 import javax.net.ssl.HttpsURLConnection;
 import org.apache.http.entity.ContentType;
@@ -49,21 +47,68 @@ public class Utils {
 		return AccessTokenConfig.getAccessToken();
 	}
 
-    public static boolean isSameAvatar(String avatar1, String avatar2)
-        throws IOException {
-	    if (avatar1 == null || !avatar1.startsWith("https://"))
-	        throw new IOException("头像路径非法");
-	    if (avatar2 == null || !avatar2.startsWith("https://"))
-            throw new IOException("头像路径非法");
-	    if (avatar1.equals(avatar2))
-	        return true;
-	    URL url1 = new URL(avatar1), url2 = new URL(avatar2);
-        HttpsURLConnection conn1 =
-            (HttpsURLConnection) url1.openConnection(), conn2 =
-            (HttpsURLConnection) url2.openConnection();
-        InputStream in1 = conn1.getInputStream(),
-            in2 = conn2.getInputStream();
-        byte[] bs1 = in1.readAllBytes(), bs2 = in2.readAllBytes();
-        return Arrays.compare(bs1, bs2) == 0;
-    }
+	public static boolean isSameAvatar(String avatar1, String avatar2)
+		throws IOException {
+		if (avatar1 == null || !avatar1.startsWith("https://")) {
+			throw new IOException("头像路径非法");
+		}
+		if (avatar2 == null || !avatar2.startsWith("https://")) {
+			throw new IOException("头像路径非法");
+		}
+		if (avatar1.equals(avatar2)) {
+			return true;
+		}
+		URL url1 = new URL(avatar1), url2 = new URL(avatar2);
+		HttpsURLConnection conn1 =
+			(HttpsURLConnection) url1.openConnection(), conn2 =
+			(HttpsURLConnection) url2.openConnection();
+		InputStream in1 = conn1.getInputStream(),
+			in2 = conn2.getInputStream();
+		byte[] bs1 = readBytes(in1), bs2 = readBytes(in2);
+		if (bs1 == null) {
+			return bs2 == null;
+		} else {
+			if (bs1.length != bs2.length) {
+				return false;
+			}
+			for (int i = 0; i < bs1.length; ++i) {
+				if (bs1[i] != bs2[i]) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+
+	public static byte[] readBytes(InputStream in) throws IOException {
+		byte[] temp = new byte[in.available()];
+		byte[] result = new byte[0];
+		int size = 0;
+		while ((size = in.read(temp)) != -1) {
+			byte[] readBytes = new byte[size];
+			System.arraycopy(temp, 0, readBytes, 0, size);
+			result = mergeArray(result, readBytes);
+		}
+		return result;
+	}
+
+	public static byte[] mergeArray(byte[]... a) {
+		// 合并完之后数组的总长度
+		int index = 0;
+		int sum = 0;
+		for (byte[] bytes : a) {
+			sum = sum + bytes.length;
+		}
+		byte[] result = new byte[sum];
+		for (byte[] bytes : a) {
+			int lengthOne = bytes.length;
+			if (lengthOne == 0) {
+				continue;
+			}
+			// 拷贝数组
+			System.arraycopy(bytes, 0, result, index, lengthOne);
+			index = index + lengthOne;
+		}
+		return result;
+	}
 }
